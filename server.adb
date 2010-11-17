4593094a48fb54068d449f350641187e46bedc29
Joseph Montanez <jmontanez@gorilla3d.com>
2010-11-17 08:04:15 -0800

    Added basic socket loop, not im late for work!

--- server.adb
+++ server.adb
@@ -17,6 +17,42 @@ procedure Server is
       return Res;
    end Reply;
 
+   task type Echo is
+      entry Start (N_Sock : in GNAT.Sockets.Socket_Type);
+   end Echo;
+
+   task body Echo is
+      Sock : GNAT.Sockets.Socket_Type;
+      S : GNAT.Sockets.Stream_Access;
+   begin
+      --   In a more realistic example we could
+      --   perform some routine initialisation here
+      --   before rendezvous with the main task
+
+      accept Start (N_Sock : in GNAT.Sockets.Socket_Type) do
+         Sock := N_Sock;
+      end Start;
+
+      S := GNAT.Sockets.Stream (Sock);
+      Boolean'Write (S, True);    -- acknowledge connection
+
+      loop
+         declare
+            Str : String := String'Input (S);
+         begin
+            exit when Str = "quit";
+            String'Output (S, Reply (Str));
+         end;
+      end loop;
+
+      Ada.Text_IO.Put_Line ("Closing Connection");
+      GNAT.Sockets.Shutdown_Socket (Sock, GNAT.Sockets.Shut_Read_Write);
+
+   exception when others =>
+         Ada.Text_IO.Put_Line ("Connection Closed");
+         GNAT.Sockets.Close_Socket (Sock);
+   end Echo;
+
    task type Socket_Task (message : Integer) is
       entry start;             --  Entry Point Into The Task
    end Socket_Task;
@@ -61,86 +97,58 @@ procedure Server is
          );
       end;
       --   TODO: This needs to be in a loop
-      begin
-         Ada.Text_IO.Put_Line ("Accepting...");
-         GNAT.Sockets.Accept_Socket (
-            Server_Socket, Client_Socket, Server_Address
-         );
-         --   TODO: Spawn another task!
-         Ada.Text_IO.Put_Line ("GAH!...");
-      exception when GNAT.Sockets.Socket_Error =>
-         Ada.Text_IO.Put_Line (
-            Ada.Text_IO.Standard_Error, "Accept_Socket raised Socket_Error"
-         );
-      end;
-      Client_Stream := GNAT.Sockets.Stream (Client_Socket);
-      String'Read (Client_Stream, Data);
-      Ada.Text_IO.Put_Line ("Streamed...");
-      Ada.Text_IO.Put_Line ("Blocking...");
-      delay 0.2;
-      Ada.Text_IO.Put_Line ("Reading channel..");
-      declare
-         message : String := String'Input (Client_Stream);
-      begin
-         Ada.Text_IO.Put_Line ("Message:");
-         Ada.Text_IO.Put_Line ("Message Addr: " & GNAT.Sockets.Image (
-            GNAT.Sockets.Get_Address (Client_Stream)
-         ));
-         Ada.Text_IO.Put_Line ("Message Text: '" & message & "'");
-         Ada.Text_IO.Put_Line ("Message Len:  " & message'Length'Img);
-      exception when Ada.Text_IO.End_Error =>
-         Ada.Text_IO.Put_Line ("Premature end of data  error");
-      end;
-      String'Write (Client_Stream,
-         "HTTP/1.1 101 WebSocket Protocol Handshake" & CR & LF &
-         "Upgrade: WebSocket" & CR & LF &
-         "Connection: Upgrade" & CR & LF &
-         "Sec-WebSocket-Origin: http://localhost" & CR & LF &
-         "Sec-WebSocket-Location: ws://localhost/demo" & CR & LF &
-         "Sec-WebSocket-Protocol: sample" & CR & LF &
-         CR & LF & CR & LF &
-         "8jKS'y:G*Co,Wxa-"
-      );
-      --   TODO: I don't really want to close sockets after one read ~_~
-      GNAT.Sockets.Close_Socket (Server_Socket);
-      GNAT.Sockets.Close_Socket (Client_Socket);
-   end Socket_Task;
-
-   task type Echo is
-      entry Start (N_Sock : in GNAT.Sockets.Socket_Type);
-   end Echo;
-
-   task body Echo is
-      Sock : GNAT.Sockets.Socket_Type;
-      S : GNAT.Sockets.Stream_Access;
-   begin
-      --   In a more realistic example we could
-      --   perform some routine initialisation here
-      --   before rendezvous with the main task
-
-      accept Start (N_Sock : in GNAT.Sockets.Socket_Type) do
-         Sock := N_Sock;
-      end Start;
-
-      S := GNAT.Sockets.Stream (Sock);
-      Boolean'Write (S, True);    -- acknowledge connection
-
       loop
          declare
-            Str : String := String'Input (S);
+            type Echo_Access is access Echo;
+            Slave : Echo_Access;
          begin
-            exit when Str = "quit";
-            String'Output (S, Reply (Str));
+            Ada.Text_IO.Put_Line ("Accepting...");
+            GNAT.Sockets.Accept_Socket (
+               Server_Socket, Client_Socket, Server_Address
+            );
+            --   TODO: Spawn another task!
+            Ada.Text_IO.Put_Line ("GAH!...");
+            Slave := new Echo;
+            Slave.start (Client_Socket);
+         exception when GNAT.Sockets.Socket_Error =>
+            Ada.Text_IO.Put_Line (
+               Ada.Text_IO.Standard_Error, "Accept_Socket raised Socket_Error"
+            );
          end;
       end loop;
 
-      Ada.Text_IO.Put_Line ("Closing Connection");
-      GNAT.Sockets.Shutdown_Socket (Sock, GNAT.Sockets.Shut_Read_Write);
-
-   exception when others =>
-         Ada.Text_IO.Put_Line ("Connection Closed");
-         GNAT.Sockets.Close_Socket (Sock);
-   end Echo;
+--      Client_Stream := GNAT.Sockets.Stream (Client_Socket);
+--      String'Read (Client_Stream, Data);
+--      Ada.Text_IO.Put_Line ("Streamed...");
+--      Ada.Text_IO.Put_Line ("Blocking...");
+--      delay 0.2;
+--      Ada.Text_IO.Put_Line ("Reading channel..");
+--      declare
+--         message : String := String'Input (Client_Stream);
+--      begin
+--         Ada.Text_IO.Put_Line ("Message:");
+--         Ada.Text_IO.Put_Line ("Message Addr: " & GNAT.Sockets.Image (
+--            GNAT.Sockets.Get_Address (Client_Stream)
+--         ));
+--         Ada.Text_IO.Put_Line ("Message Text: '" & message & "'");
+--         Ada.Text_IO.Put_Line ("Message Len:  " & message'Length'Img);
+--      exception when Ada.Text_IO.End_Error =>
+--         Ada.Text_IO.Put_Line ("Premature end of data  error");
+--      end;
+--      String'Write (Client_Stream,
+--         "HTTP/1.1 101 WebSocket Protocol Handshake" & CR & LF &
+--         "Upgrade: WebSocket" & CR & LF &
+--         "Connection: Upgrade" & CR & LF &
+--         "Sec-WebSocket-Origin: http://localhost" & CR & LF &
+--         "Sec-WebSocket-Location: ws://localhost/demo" & CR & LF &
+--         "Sec-WebSocket-Protocol: sample" & CR & LF &
+--         CR & LF & CR & LF &
+--         "8jKS'y:G*Co,Wxa-"
+--      );
+--      --   TODO: I don't really want to close sockets after one read ~_~
+--      GNAT.Sockets.Close_Socket (Server_Socket);
+--      GNAT.Sockets.Close_Socket (Client_Socket);
+   end Socket_Task;
 
    Socket : Socket_Task (message => 1);
 begin
